75c23895024f3bb93ecd159abe297f8cff1c38d5
Einas Haddad <einas@haddad>
2013-03-04 14:15:34 -0500

    Code review changes

--- boggle.py
+++ boggle.py
@@ -3,110 +3,62 @@
 Implementation of a Boggle cheater
 
 Takes in a string of letters from the command line and 
-returns the different different words that can be created
+returns the different words that can be created
 using those letters
 """
 
 import itertools
-import argparse
+import sys
 import boggle_graph
 
 """
-Parses the arguments from the command line and calls the function boggle
+Parses the arguments from the command line
 """
-parser = argparse.ArgumentParser()
-parser.add_argument("board", nargs = 1, help="a string of characters (nxn board)")
-parser.add_argument("n", nargs = 1, help="n where n is the width or height of the board")
-args = parser.parse_args()
-board = args.board[0]
-n = int(args.n[0])
+letters = sys.argv[1]
+n = int(sys.argv[2])
+if len(sys.argv) == 4:
+    dictionary_path = sys.argv[3]
+else:
+    dictionary_path = '/usr/share/dict/words'
 
-visited = {}
-
-def create_dictionary(dictionary='/usr/share/dict/words'):
+def create_dictionary(dictionary = dictionary_path):
     """
-    Imports words from a local dictionary into a python dictionary
+    Imports words from a local dictionary into a python set
     """
-    with open(dictionary, 'r') as f:
-        return {line.strip() for line in f.readlines()}
- 
-def power_set(permutations, n):
+    try:
+        with open(dictionary, 'r') as f:
+            return {line.strip().lower() for line in f.readlines()}
+    except IOError:
+        print "No dictionary at /usr/share/dict/words. Please enter path to dictionary at command line." 
+        sys.exit()
+
+def dfs(visited_nodes, graph, node=('', (None, None))):
     """
-    Returns the set of permutations of length n from the power set of the elements
-    """
-    results = []
+    Returns words of length 3 or more on the board that are present in the dictionary
+    """    
+    global dictionary
+    visited_nodes = visited_nodes + [node]
 
-    for perm in permutations:
-        results.append(perm[:n])
-    return set(results)
+    word_fragment = "".join([letter for letter, position in visited_nodes])
 
-def permutation(elements, length):
-    """
-    Returns all permutations of the given elements
-    """
+    if len(word_fragment) >= 3 and word_fragment in dictionary:
+        yield word_fragment
 
-    # return set(itertools.permutations(board, length))
+    good_neighbors = [n for n in graph[node] if n not in visited_nodes]
 
-    result = []
+    for neighbor in good_neighbors:
+        for result in dfs(visited_nodes, graph, neighbor):
+            yield result
 
-    #base case
-    if len(elements) <= 1:
-        return elements
+if __name__ == "__main__":
+    dictionary = create_dictionary()
+    print "dictionary created"
 
-    #recursive case
-    for i, element in enumerate(elements):
-        for perm in permutation(elements[:i]+elements[i+1:], length):
-            result.append(element + perm)
-    return power_set(result, length)
+    board, position = boggle_graph.make_board(letters, n)
+    graph = boggle_graph.make_graph(board, position)
+    words_out = []
+    for word in dfs([], graph):
+        words_out.append(word)
 
-def boggle(board):
-    """
-    Prints all permutations that are availbale in the dictionary
-    """
-    result = []
-    words = create_dictionary()
-    
-    # Iterates through all possible lengths (words of length 3 - length of board)
-    for i in xrange(3, len(board)+1):
-        permutations = permutation(board, i)
-        perm = []
-        for p in permutations:  
-            perm.append(''.join(elem[0] for elem in p)) # Joins letters into one string
-        for item in (perm):
-            if item.lower() in words:
-                result.append(item.lower())
-    return result
-
-def dfs(graph, word, node=' '):
-    """
-    Performs depth-first-search on the graph, returns True if the word is present
-    """
-    global visited
-    # base case
-    if word[0] in node and len(word) <= 1:
-        return True
-    if word[0] not in node:
-        return False
-    # recursive step
-    visited[(node)] = True 
-    for neighbor in graph[node]:
-        if not visited.get((neighbor), False):
-            if dfs(graph, word[1:], neighbor):
-                return True
-    visited[(node)] = False
-    return False
-
-permutations = boggle(board)  
-board, position = boggle_graph.make_board(board, n)
-graph = boggle_graph.make_graph(board, position)
-for word in permutations: 
-    visited = {}
-    if dfs(graph, ' '+word):
-        print word
-
-
-
-
-
-
-
+    print words_out
+    print len(words_out)
--- boggle_graph.py
+++ boggle_graph.py
@@ -2,19 +2,19 @@
 Generate a graph of the boggle board
 """
 
-def make_board(B, n):
+def make_board(letters, board_dimensions):
 	"""
 	Returns a 2D matrix of the n x n board that is padded with None 
 	and returns a dictionary of the positioning of the letters on the board 
 	"""
-	board = [[None for i in xrange(n+2)] for i in xrange(n+2)]
+	board = [[None for i in xrange(board_dimensions+2)] for i in xrange(board_dimensions+2)]
 	position = {}
 	x = 0
 	y = 0
-	for letter in (' '.join(B)).split():
+	for letter in letters:
 		position[x, y] = letter.lower()
 		y += 1
-		if y >= n:
+		if y >= board_dimensions:
 			y = 0
 			x += 1
 
@@ -22,21 +22,20 @@ def make_board(B, n):
 		board[x+1][y+1] = position[(x,y)]
 	return board, position
 
-def get_neighbors(node, board, position):
+def get_neighbors(board, current_position):
 	"""
 	Returns a list of the reachable neighbors of a certain node at position (x,y) on the board
 	"""
 	result = []
-	x, y = position[0], position[1]
+	x, y = current_position
 	a, b = x-1, y-1
 	neighbors = [(board[x-1][y-1], (a-1,b-1)), (board[x-1][y], (a-1,b)), \
 				(board[x-1][y+1], (a-1, b+1)), (board[x][y-1], (a, b-1)), \
 				(board[x][y+1], (a,b+1)), (board[x+1][y-1], (a+1, b-1)), \
 				(board[x+1][y], (a+1, b)), (board[x+1][y+1], (a+1, b+1))]
-	for neighbor in neighbors:
-		if neighbor[0]:
-			result.append(neighbor)
-	return result
+
+	neighbors = [n for n in neighbors if n[0]]
+	return neighbors
 
 def make_graph(board, position):
 	"""
@@ -44,9 +43,8 @@ def make_graph(board, position):
 	as the values
 	"""
 	graph = {}
-	graph[' '] = []
 	for node in position:
-		graph[(position[node], node)] = get_neighbors(position[node], board, (node[0]+1, node[1]+1))
-		graph[' '].append((position[node], node))
+		graph[(position[node], node)] = get_neighbors(board, (node[0]+1, node[1]+1))
+	graph[('', (None, None))] = [(position[node], node) for node in position]  # wildcard node - can start at any point on board
 	return graph	
 
